faede96a7e0a43020cc502cba32c7a70352757e3
prezgamer <ffjandrew@yahoo.com.sg>
2025-01-12 15:49:24 +0800

    2301007-Lab_1
    Completed test cases for Lab-1
    testGreetingAppearsOnSubmit
    testGreetingDoesNotAppearForBlankInput
    mainScreenTest
    testGreetingUpdatesWithNewInput

--- app/src/main/java/com/inf2007/lab01/MainActivity.kt
+++ app/src/main/java/com/inf2007/lab01/MainActivity.kt
@@ -34,12 +34,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun MainScreen() {
     Lab01Theme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             var username by remember { mutableStateOf("") }
             var showGreeting by remember { mutableStateOf(false) }
+            var greetingName by remember { mutableStateOf("") }
 
             Column(
                 modifier = Modifier
@@ -49,13 +51,18 @@ fun MainScreen() {
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 UserInput(
-                    name = name,
-                    onNameChange = { name = it }
+                    name = username,
+                    onNameChange = {
+                        username = it
+                    }
                 )
 
                 Button(
                     onClick = {
                         if (username.isNotBlank()) {
+                            showGreeting = true
+                            greetingName = username
+                        } else {
                             showGreeting = false
                         }
                     },
@@ -67,8 +74,8 @@ fun MainScreen() {
                 }
 
                 if (showGreeting) {
-                    Greeeting(
-                        name = username,
+                    Greeting(
+                        name = greetingName,
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(top = 16.dp)
@@ -88,17 +95,17 @@ fun UserInput(name: String, onNameChange: (String) -> Unit, modifier: Modifier =
         label = { Text("Enter your Name") },
         modifier = modifier
             .fillMaxWidth()
-            .testTag("UserInput")
+            .testTag("nameInput")
     )
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $username!, Welcome to InF2007!",
+        text = "Hello $name!, Welcome to INF2007!",
         modifier = Modifier
             .fillMaxWidth()
-            .testTag("greeting")
+            .testTag("greetingMsg")
     )
 }
 
